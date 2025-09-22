c0ae216c19e39bfa731a794f88ffb1c97e18e878
tpganime <139879328+tpganime@users.noreply.github.com>
2025-09-23 02:27:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const items = [
-  {title:"spider-man 2", image:"https://share.google/images/UuYsK0A8SmmZQaooz", id:"sortalcreel1582", password:"sortalcreel1582"},
-  {title:"cyberpunk 2077", image:"https://share.google/images/TGwzhQE9a0AoVMWlt", password:"aegis084"},
-  {title:"GTA V", image:"https://share.google/images/VCY82Q7iYU9ZeqLHV", password:"Xbox_360!"},
-  {title:"Black myth wukong", image:https://share.google/images/zfAaIPBrK4yb97PNk", id:"tazrs41035", password:"ibwe69388M"},
+  {title:"Spider-Man 2", image:"https://share.google/images/UuYsK0A8SmmZQaooz", id:"sortalcreel1582", password:"sortalcreel1582"},
+  {title:"Cyberpunk 2077", image:"https://share.google/images/TGwzhQE9a0AoVMWlt", id:"", password:"aegis084"},
+  {title:"GTA V", image:"https://share.google/images/VCY82Q7iYU9ZeqLHV", id:"", password:"Xbox_360!"},
+  {title:"Black Myth Wukong", image:"https://share.google/images/zfAaIPBrK4yb97PNk", id:"tazrs41035", password:"ibwe69388M"},
   {title:"FORZA HORIZON 4", image:"https://share.google/images/sXbAiictSBMHOqLdb", id:"bejwc", password:"aA102020"},
   {title:"Resident Evil 4", image:"https://share.google/images/k5MkrQJ7L3qc2Td4Z", id:"7854YIxK", password:"6399dnEVfh13"},
   {title:"Hollow Knight: Silksong", image:"https://share.google/images/LP8k3Y21S1wvIZM0y", id:"HYsh23s8", password:"vb501Iak"},
@@ -19,8 +19,8 @@ items.forEach(item => {
     <div class="info">
       <h4>${item.title}</h4>
       <div class="credentials">
-        <input type="text" value="${item.id}" readonly>
-        <input type="text" value="${item.password}" readonly>
+        <input type="text" value="${item.id}" readonly placeholder="No ID">
+        <input type="text" value="${item.password}" readonly placeholder="No Password">
         <button>Copy</button>
       </div>
     </div>
@@ -28,7 +28,7 @@ items.forEach(item => {
   const btn = card.querySelector("button");
   btn.onclick = () => {
     const inputs = card.querySelectorAll("input");
-    const text = Array.from(inputs).map(i => i.value).join(" / ");
+    const text = Array.from(inputs).map(i => i.value).filter(v=>v).join(" / ");
     navigator.clipboard.writeText(text);
     btn.textContent = "Copied!";
     setTimeout(() => btn.textContent = "Copy", 1000);
